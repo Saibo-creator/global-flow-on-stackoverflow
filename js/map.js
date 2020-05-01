18dc1863f0ccac1097d93f93ade21d08f469beec
Saibo-creator <saibo.geng@yahoo.fr>
2020-05-01 13:46:13 +0800

    minimal viable website

--- js/map.js
+++ js/map.js
@@ -12,23 +12,25 @@ var infoWidth = $('body').width();
 // DEFINE FUNCTIONS/OBJECTS
 // Define map projection
 var projection = d3.geoEquirectangular()
-                   .center([0, 15]) // set centre to further North as we are cropping more off bottom of map
-                   .scale([w / (2 * Math.PI)]) // scale to fit group width
-                   .translate([(w- 0.64*infoWidth) / 2, h / 2]); // ensure centred in group
+    .center([0, 15]) // set centre to further North as we are cropping more off bottom of map
+    .scale([w / (2 * Math.PI)]) // scale to fit group width
+    .translate([(w - 0.64 * infoWidth) / 2, h / 2]); // ensure centred in group
 
 // Define map path
 var path = d3.geoPath()
-             .projection(projection);
+    .projection(projection);
 
 // Create function to apply zoom to countriesGroup
 function zoomed() {
     t = d3.event.transform;
-    countriesGroup.attr("transform","translate(" + [t.x, t.y] + ")scale(" + t.k + ")");
+    countriesGroup.attr("transform", "translate(" + [t.x, t.y] + ")scale(" + t.k + ")");
+    countryLabelGroup.attr("transform", "translate(" + [t.x, t.y] + ")scale(" + t.k + ")");
+    flow.attr("transform", "translate(" + [t.x, t.y] + ")scale(" + t.k + ")");
 }
 
 // Define map zoom behaviour
 var zoom = d3.zoom()
-             .on("zoom", zoomed);
+    .on("zoom", zoomed);
 
 function getTextBox(selection) {
     selection.each(function(d) {
@@ -46,7 +48,10 @@ function initiateZoom() {
     // set extent of zoom to chosen values
     // set translate extent so that panning can't cause map to move out of viewport
     zoom.scaleExtent([minZoom, maxZoom])
-        .translateExtent([[0, 0], [w, h]]);
+        .translateExtent([
+            [0, 0],
+            [w, h]
+        ]);
     // define X and Y offset for centre of map to be shown in centre of holder
     midX = ($("#map-holder").width() - minZoom * w) / 2;
     midY = ($("#map-holder").height() - minZoom * h) / 2;
@@ -54,146 +59,271 @@ function initiateZoom() {
     svg.call(zoom.transform, d3.zoomIdentity.translate(midX, midY).scale(minZoom));
 }
 
+
+
+
+
+//function used to sort data based on value
+function compareValues(key, order = 'asc') {
+    return function innerSort(a, b) {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            // property doesn't exist on either object
+            return 0;
+        }
+
+        const varA = (typeof a[key] === 'string') ?
+            a[key].toUpperCase() : a[key];
+        const varB = (typeof b[key] === 'string') ?
+            b[key].toUpperCase() : b[key];
+
+        let comparison = 0;
+        if (varA > varB) {
+            comparison = 1;
+        } else if (varA < varB) {
+            comparison = -1;
+        }
+        return (
+            (order === 'desc') ? (comparison * -1) : comparison
+        );
+    };
+}
+
+
+//function to create flow with animation
+function createFlow(iso, flows) {
+
+    console.log("flows=", flows);
+    flows = flows.filter(function(v) { return v.ans_owner_country == iso }).slice(0, 20);
+    flow.selectAll("line")
+        .data(flows)
+        // .filter(function(d) { return d.ans_owner_country == iso; })
+        // //shows only the biggest 20 flows
+        // .filter(function(d, i) { return i < 20; })
+        .enter()
+        .append("line")
+        .attr('class', 'line')
+        .attr("x1", d => projection([d.long_ques, d.lat_ques])[0])
+        .attr("y1", d => projection([d.long_ques, d.lat_ques])[1])
+        .attr("x2", d => projection([d.long_ques, d.lat_ques])[0])
+        .attr("y2", d => projection([d.long_ques, d.lat_ques])[1])
+        .attr("stroke-width", 1)
+        .attr("stroke", "pink");
+    //.attr("marker-end", "url(#arrow)");
+    //arrow def
+
+
+
+    flow.selectAll("line")
+        .data(flows)
+        .filter(function(d) { return d.ans_owner_country == iso; })
+        //shows only the biggest 20 flows
+        .filter(function(d, i) { return i < 20; })
+        .transition()
+        .duration(1000)
+        .attr("x1", d => projection([d.long_ques, d.lat_ques])[0])
+        .attr("y1", d => projection([d.long_ques, d.lat_ques])[1])
+        .attr("x2", d => projection([d.long_ans, d.lat_ans])[0])
+        .attr("y2", d => projection([d.long_ans, d.lat_ans])[1])
+        .attr("stroke-width", d => (
+            d.count / 4000 > 2 ? d.count / 4000 : 2))
+        // })(order === 'desc') ? (comparison * -1) : comparison
+        .attr("stroke", "pink");
+    //.attr("marker-end", "url(#arrow2)");
+    for (var i = 0; i < flows.length; i++) {
+        iso = flows[i].ques_owner_country;
+        d3.select("#countryLabel" + iso).style("display", "block");
+        if (iso!=flows[i].ans_owner_country) {
+            d3.select("#countryLabel" + iso).select('text').attr('class', 'countryNameSourceFlow');
+    }
+        }
+
+        
+
+
+
+
+}
+
+
 // on window resize
 $(window).resize(function() {
     // Resize SVG
     svg.attr("width", $("#map-holder").width())
-       .attr("height", $("#map-holder").height());
+        .attr("height", $("#map-holder").height());
     initiateZoom();
 });
 
 // create an SVG
 var svg = d3.select("#map-holder")
-            .append("svg")
-            // set to the same size as the "map-holder" div
-            .attr("width", $("#map-holder").width())
-            .attr("height", $("#map-holder").height())
-            // add zoom functionality
-            .call(zoom);
+    .append("svg")
+    // set to the same size as the "map-holder" div
+    .attr("width", $("#map-holder").width())
+    .attr("height", $("#map-holder").height())
+    // add zoom functionality
+    .call(zoom);
 
-var top50 = ['USA','IND','GBR','DEU','CHN','CAN','BRA','FRA','RUS','AUS','PAK','POL','NLD','ESP','IDN','ITA','TUR','PHL','VNM','BGD','IRN',
-             'UKR','SWE','EGY','MEX','ISR','ZAF','KOR','ROU','NGA','BEL','CHE','ARG','SGP','LKA','JPN','MYS','PRT','DNK','IRL','AUT',
-             'COL','GRC','NZL','MAR','NOR','NPL','THA','CZE','HUN']
+var top50 = ['USA', 'IND', 'GBR', 'DEU', 'CHN', 'CAN', 'BRA', 'FRA', 'RUS', 'AUS', 'PAK', 'POL', 'NLD', 'ESP', 'IDN', 'ITA', 'TUR', 'PHL', 'VNM', 'BGD', 'IRN',
+    'UKR', 'SWE', 'EGY', 'MEX', 'ISR', 'ZAF', 'KOR', 'ROU', 'NGA', 'BEL', 'CHE', 'ARG', 'SGP', 'LKA', 'JPN', 'MYS', 'PRT', 'DNK', 'IRL', 'AUT',
+    'COL', 'GRC', 'NZL', 'MAR', 'NOR', 'NPL', 'THA', 'CZE', 'HUN'
+]
+var countriesGroup = svg.append("g").attr("id", "countriesGroup");
+
+var flow = svg.append('g')
+    .attr('id', 'flow');
+
+var circle = svg.append("g")
+    .attr("id", "circle");
+
+var barChart = svg.append("g")
+    .attr("id", "barChart");
+
+countryLabelGroup = svg.append("g").attr("id", "countryLabelGroup");
+
 
 $(document).ready(function() {
     //get country data
     var countryData;
     $.getJSON("data/country_data.json", function(data) {
         countryData = data;
-      })
+    });
+
+    d3.csv("./data/flow_betw_country.csv", function(data) {
+        flows = data;
+        flows.forEach(function(d) { d.count = +d.count; });
+
+        // array is sorted by user in descending order
+        flows.sort(compareValues('count', order = 'desc'));
+        //Bind data and create one path per GeoJSON feature
+
+    });
 
     // get map data
     d3.json("data/world.geo.json", function(json) {
         //Bind data and create one path per GeoJSON feature
-        countriesGroup = svg.append("g").attr("id", "map");
         // add a background rectangle
         countriesGroup.append("rect")
-                    .attr("x", 0)
-                    .attr("y", 0)
-                    .attr("width", w)
-                    .attr("height", h);
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", w)
+            .attr("height", h);
 
         // draw a path for each feature/country
         countries = countriesGroup.selectAll("path")
-                                .data(json.features)
-                                .enter()
-                                .append("path")
-                                .attr("d", path)
-                                .attr("id", function(d, i) {
-                                    return "country" + d.properties.iso_a3;
-                                })
-                                .attr("class", "country")
-                                .attr("style", function(d, i) {
-                                    return getCountryStyleString(d, i);
-                                })
-                            //      .attr("stroke-width", 10)
-                            //      .attr("stroke", "#ff0000")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("id", function(d, i) {
+                return "country" + d.properties.iso_a3;
+            })
+            .attr("class", "country")
+            .attr("style", function(d, i) {
+                return getCountryStyleString(d, i);
+            })
+            //      .attr("stroke-width", 10)
+            //      .attr("stroke", "#ff0000")
 
-                                // add a mouseover action to show name label for feature/country
-                                .on("mouseover", function(d, i) {
-                                    d3.select("#countryLabel" + d.properties.iso_a3).style("display", "block");
-                                })
-                                .on("mouseout", function(d, i) {
-                                    d3.select("#countryLabel" + d.properties.iso_a3).style("display", "none");
-                                })
-                                // add an onclick action to zoom into clicked country
-                                .on("click", function(d, i) {
-                                    d3.selectAll(".country").classed("country-on", false);
-                                    d3.select(this).classed("country-on", true);
-                                    if(d.properties.iso_a3 in countryData){
-                                        let cData = countryData[d.properties.iso_a3]
-                                        showStat(d,cData);
-                                    }
-                                    else{
-                                        showStat(d,null);
-                                    }
-                                });
-        
+            // add a mouseover action to show name label for feature/country
+            .on("mouseover", function(d, i) {
+                d3.select("#countryLabel" + d.properties.iso_a3).style("display", "block");
+            })
+            .on("mouseout", function(d, i) {
+                d3.select("#countryLabel" + d.properties.iso_a3).style("display", "none");
+            })
+            // add an onclick action to zoom into clicked country
+            .on("click", function(d, i) {
+                d3.selectAll(".country").classed("country-on", false);
+                d3.select(this).classed("country-on", true);
+                if (d.properties.iso_a3 in countryData) {
+                    let cData = countryData[d.properties.iso_a3]
+                    showStat(d, cData);
+                } else {
+                    showStat(d, null);
+                }
+                                //add flow effect
+                flow.selectAll("line").remove();
+                createFlow(d.properties.iso_a3, flows);
+            });
+
+
+
         // Add a label group to each feature/country. This will contain the country name and a background rectangle
         // Use CSS to have class "countryLabel" initially hidden
-        countryLabels = countriesGroup.selectAll("g")
-                                    .data(json.features)
-                                    .enter()
-                                    .append("g")
-                                    .attr("class", "countryLabel")
-                                    .attr("id", function(d) {
-                                        return "countryLabel" + d.properties.iso_a3;
-                                    })
-                                    .attr("transform", function(d) {
-                                        if(d.properties.iso_a3 == "USA"){
-                                            return (
-                                                "translate(" + (path.centroid(d)[0] + 100) + "," + (path.centroid(d)[1] +50) + ")"
-                                                );
-                                        }
-                                        else{
-                                            return (
-                                                "translate(" + path.centroid(d)[0] + "," + path.centroid(d)[1] + ")"
-                                                );
-                                        }
-                                    })
-                                    // add mouseover functionality to the label
-                                    .on("mouseover", function(d, i) {
-                                        d3.select(this).style("display", "block");
-                                    })
-                                    .on("mouseout", function(d, i) {
-                                        d3.select(this).style("display", "none");
-                                    })
-                                    // add an onlcick action to zoom into clicked country
-                                    .on("click", function(d, i) {
-                                        d3.selectAll(".country").classed("country-on", false);
-                                        d3.select("#country" + d.properties.iso_a3).classed("country-on", true);
-                                        if(d.properties.iso_a3 in countryData){
-                                            let cData = countryData[d.properties.iso_a3]
-                                            showStat(d,cData);
-                                        }
-                                        else{
-                                            showStat(d,null);
-                                        }
-                                        
-                                    });
+
+
+        countryLabels = countryLabelGroup.selectAll("g")
+            .data(json.features)
+            .enter()
+            .append("g")
+            .attr("class", "countryLabel")
+            .attr("id", function(d) {
+                return "countryLabel" + d.properties.iso_a3;
+            })
+            .attr("transform", function(d) {
+                if (d.properties.iso_a3 == "USA") {
+                    return (
+                        "translate(" + (path.centroid(d)[0] + 100) + "," + (path.centroid(d)[1] + 50) + ")"
+                    );
+                } else {
+                    return (
+                        "translate(" + path.centroid(d)[0] + "," + path.centroid(d)[1] + ")"
+                    );
+                }
+            })
+            // add mouseover functionality to the label
+            .on("mouseover", function(d, i) {
+                d3.select(this).style("display", "block");
+            })
+            .on("mouseout", function(d, i) {
+                    d3.select(this).style("display", "none");
+                
+            })
+            // add an onlcick action to zoom into clicked country
+            .on("click", function(d, i) {
+                d3.selectAll(".country").classed("country-on", false);
+                d3.select("#country" + d.properties.iso_a3).classed("country-on", true);
+                if (d.properties.iso_a3 in countryData) {
+                    let cData = countryData[d.properties.iso_a3]
+                    showStat(d, cData);
+                } else {
+                    showStat(d, null);
+                }
+
+                //add flow effect
+                flow.selectAll("line").remove();
+                createFlow(d.properties.iso_a3, flows);
+
+
+            });
         // add the text to the label group showing country name
         countryLabels.append("text")
-                    .attr("class", "countryName")
-                    .style("text-anchor", "middle")
-                    .attr("dx", 0)
-                    .attr("dy", 0)
-                    .text(function(d) {
-                        return d.properties.name;
-                    })
-                    .call(getTextBox);
-        
+            .attr("class", "countryName")
+            .style("text-anchor", "middle")
+            .attr("dx", 0)
+            .attr("dy", 0)
+            .text(function(d) {
+                return d.properties.name;
+            })
+            .call(getTextBox);
+
         // add a background rectangle the same size as the text
         countryLabels.insert("rect", "text")
-                    .attr("class", "countryLabelBg")
-                    .attr("transform", function(d) {
-                        return "translate(" + (d.bbox.x - 2) + "," + d.bbox.y + ")";
-                    })
-                    .attr("width", function(d) {
-                        return d.bbox.width + 4;
-                    })
-                    .attr("height", function(d) {
-                        return d.bbox.height;
-                    });
+            .attr("class", "countryLabelBg")
+            .attr("transform", function(d) {
+                return "translate(" + (d.bbox.x - 2) + "," + d.bbox.y + ")";
+            })
+            .attr("width", function(d) {
+                return d.bbox.width + 4;
+            })
+            .attr("height", function(d) {
+                return d.bbox.height;
+            });
+
+
+
+
+
+
         initiateZoom();
     });
 });
