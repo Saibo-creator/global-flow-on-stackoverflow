b6c7df481fe5a4364c82de38f0d7ab8aa7c6e20b
Saibo-creator <saibo.geng@yahoo.fr>
2020-05-22 21:31:01 +0800

    added animation for progress bar

--- js/stats-info.js
+++ js/stats-info.js
@@ -45,16 +45,17 @@ function appendFlowStat(iso, flows ) {
         $('#progress-bar-large').html('<div id="progress-bar-large"><p class="hbar-title">Top5 flows</p>');
         $('#progress-bar-large').append('<div id="progress-bar">');
         largest_flow_count = flows[0].count;
-        progress_bar_class = ["progress-bar progress-bar-striped progress-bar-animated", "progress-bar-animated progress-bar-striped bg-success", "progress-bar-animated progress-bar-striped bg-info", "progress-bar-animated progress-bar-striped bg-warning", "progress-bar-animated progress-bar-striped bg-danger"]
+        progress_bar_class = ["progress-bar progress-bar-animated progress-bar-striped", "progress-bar progress-bar-animated progress-bar-striped bg-success", "progress-bar progress-bar-animated progress-bar-striped bg-info", "progress-bar progress-bar-animated progress-bar-striped bg-warning", "progress-bar progress-bar-animated progress-bar-striped bg-danger"]
         flows.forEach(function(flow, i) {
             if (i <= 4) {
                 $('#progress-bar').append('<div style="margin-top:30px;margin-bottom: 0 px; ">' + flow.ques_owner_country + ':' + flow.count + '</div>')
                 $('#progress-bar').append('<div class="progress progress-striped active">' +
-                    '<div ' + 'id=' + i + ' role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:' + flow.count / largest_flow_count * 100 + '%' + ';">' +
-                    '<span class="sr-only">90% 完成（成功）</span>' +
+                    '<div ' + 'id=' + i + ' role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:' + "0%;>"+
+                    '<span class="sr-only">'+(flow.count / largest_flow_count * 100).toFixed(1) + '%'+'</span>' +
                     '</div>' +
                     '</div>');
                 $('#' + i).addClass(progress_bar_class[i]);
+                $('#' + i).animate({width: (flow.count / largest_flow_count * 100).toFixed(1) + '%'  }, 1000);
 
             }
 
