558623716a8916f54689359e966002177b5a4c87
yukixLLL <yueran.liang@epfl.ch>
2020-05-24 19:46:03 +0200

    population over years & some style

--- js/stats-info.js
+++ js/stats-info.js
@@ -1,4 +1,4 @@
-function showStat(c, d) {
+function showStat(c, d,worldData,surveyData) {
     $('#countryName-holder').html('<i id="country-flag" ></i> <p class=\'country-name\'>' + c.properties.name + '</p>');
     var classes=[(c.properties.name.toLowerCase()),'flag']
     $("#country-flag").addClass(classes.join(" "));
@@ -12,12 +12,69 @@ function showStat(c, d) {
 
     direc = localStorage.getItem("direction");
     console.log(direc);
+    console.log(surveyData)
+    var country_name = c.properties.name
     if (d != null) {
-        $('#statistics-holder').html('<p>' + c.properties.name + '\'s statistic</p>' + '<br>' +
-            '<p> The number of users is ' + d['user'] + '</p>' + '<br>' +
-            '<p> The number of questions is ' + d['ques'] + '</p>' + '<br>' +
-            '<p> The number of answers is ' + d['ans'] + '</p>'
+        $('#statistics-holder').html('<div class=\"country-statistic\">'+
+            '<div class=\"user_container\"> <span class=\"num_indicator\"><i class=\"fa fa-users\" aria-hidden=\"true\" style=\"color:white\"></i>&nbsp;&nbsp;Users</span><span class=\"num\">' + d['user'] + '</span></div>' +
+            '<div class=\"num_container\">'+
+            '<div> <span class=\"num_indicator\"><i class=\"fa fa-question-circle\" aria-hidden=\"true\" style=\"color:white\"></i>&nbsp;&nbsp;Questions</span> <span class=\"num\">' + d['ques'] + '</span></div>' + 
+            '<div> <span class=\"num_indicator\"><i class=\"fa fa-comments-o\" aria-hidden=\"true\" style=\"color:white\"></i>&nbsp;&nbsp;Answers</span><span class=\"num\">' + d['ans'] + '</span></div>'+ 
+            '</div>'+
+            '</div>'
         );
+        $('#statistics-holder').append("<div id=\"userChangeLineChart\" ></div>")
+        chartHeight = $('#userChangeLineChart').height()
+        chartWidth = $('#userChangeLineChart').width()
+        var chart = new CanvasJS.Chart("userChangeLineChart", {
+            animationEnabled: true,
+            width: chartWidth,
+            height: chartHeight,
+            // theme: "light2",
+            backgroundColor: null,
+            title:{
+                text: "Users' Population Over Years",
+                fontSize: 25,
+                fontWeight:"normal",
+                fontFamily: "cursive",
+                fontColor: "white",
+            },
+            axisX: {
+                title: "Year",
+                lineColor: "white",
+                labelFontColor: "white",
+                titleFontColor: "white",
+                valueFormatString:"####"
+            },
+            axisY:{
+                includeZero: false,
+                title: "Population",
+                gridColor: "#f5f5f5",
+                lineColor: "#f5f5f5",
+                labelFontColor: "white",
+                titleFontColor: "white"
+            },
+            data: [{        
+                type: "line",
+                lineColor: "#ebe306",
+                color:"#ff7373",
+                indexLabelFontSize: 10,
+                xValueFormatString:"Year ####",
+                dataPoints: [
+                { x: 2014, y: surveyData[0] },
+                { x: 2015, y: surveyData[1] },
+                { x: 2016, y: surveyData[2] },
+                { x: 2017, y: surveyData[3] },
+                { x: 2018, y: surveyData[4] },
+                { x: 2019, y: surveyData[5] },
+                ]
+            }]
+        });
+        chart.render();
+
+
+
+        
     } else {
         $('#statistics-holder').html("There is no information for this country.");
     }
@@ -33,14 +90,14 @@ function showStat(c, d) {
 function appendFlowStat(iso, flows ) {
     flows = flows.filter(function(v) { return v.ans_owner_country == iso; }).slice(0, 20);
     if (flows != null) {
-        $('#statistics-holder').append(flows[0].ans_owner_country,
-            "'s top20 most conneccetd user communities are(in descending order): ");
-        flows.forEach(function(flow, i) {
-            $('#statistics-holder').append(flow.ques_owner_country, ', ');
+        // $('#statistics-holder').append("<p>" + flows[0].ans_owner_country +
+        //     "'s top20 most conneccetd user communities are(in descending order):</p> ");
+        // flows.forEach(function(flow, i) {
+        //     $('#statistics-holder').append(flow.ques_owner_country, ', ');
 
-        });
-        $('#statistics-holder').append('</p>' + '<br>');
-        $('#statistics-holder').append();
+        // });
+        // $('#statistics-holder').append('</p>' + '<br>');
+        // $('#statistics-holder').append();
 
         $('#progress-bar-large').html('<div id="progress-bar-large"><p class="hbar-title">Top5 flows</p>');
         $('#progress-bar-large').append('<div id="progress-bar">');
@@ -48,7 +105,7 @@ function appendFlowStat(iso, flows ) {
         progress_bar_class = ["progress-bar progress-bar-animated progress-bar-striped", "progress-bar progress-bar-animated progress-bar-striped bg-success", "progress-bar progress-bar-animated progress-bar-striped bg-info", "progress-bar progress-bar-animated progress-bar-striped bg-warning", "progress-bar progress-bar-animated progress-bar-striped bg-danger"]
         flows.forEach(function(flow, i) {
             if (i <= 4) {
-                $('#progress-bar').append('<div style="margin-top:30px;margin-bottom: 0 px; ">' + flow.ques_owner_country + ':' + flow.count + '</div>')
+                $('#progress-bar').append('<div style="margin-top:20px;margin-bottom: 0 px; ">' + flow.ques_owner_country + ':' + flow.count + '</div>')
                 $('#progress-bar').append('<div class="progress progress-striped active">' +
                     '<div ' + 'id=' + i + ' role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:' + "0%;>"+
                     '<span class="sr-only">'+(flow.count / largest_flow_count * 100).toFixed(1) + '%'+'</span>' +
